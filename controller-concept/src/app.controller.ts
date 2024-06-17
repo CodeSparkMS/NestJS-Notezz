1286c8cac912a63cf70e5e6eeb8e62fe30b6d819
SaimMohanish <mohanish.saim@zingworks.in>
2024-06-17 18:10:47 +0530

    - Learning the concept of services - Creating object of services or providers - Use of @Injectable decorator

--- controller-concept/src/app.controller.ts
+++ controller-concept/src/app.controller.ts
@@ -1,38 +1,52 @@
-import { Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BookService } from './app.service';
 
 @Controller('book')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly bookService: BookService) {}
+  // public bookService: BookService = new BookService()
+  constructor(public bookService: BookService) {}
 
   @Get()
   @HttpCode(200)
   getHello(): string {
-    return this.appService.getHello();
+    return this.bookService.getHello();
   }
 
   // Add Book
   @Post('/add')
   addbook(): string {
-    return 'New Book Added';
+    // return 'New Book Added';
+    return this.bookService.addbook();
   }
 
   // Delete Book
   @Delete('/delete')
   deleteBook(): string {
-    return 'Book deleted successfully';
+    // return 'Book deleted successfully';
+    return this.bookService.deleteBook();
   }
 
   // Update Book
   @Put('/update')
   updateBook(): string {
-    return 'Book updated successfully';
+    // return 'Book updated successfully';
+    return this.bookService.updateBook();
   }
 
   // Get All Book
   @Get('/get')
   getAllBook(): string {
-    return 'Get all books';
+    // return 'Get all books';
+    return this.bookService.getAllBook();
   }
 
   // Find Book By Id
--- controller-concept/src/app.service.ts
+++ controller-concept/src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class BookService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  addbook(): string {
+    return 'New Book Added';
+  }
+
+  deleteBook(): string {
+    return 'Book deleted successfully';
+  }
+
+  updateBook(): string {
+    return 'Book updated successfully';
+  }
+
+  getAllBook(): string {
+    return 'Get all books';
+  }
+
 }
